125496b6a17c6e27347a258d9b8da100eee881cf
blueyorange <blueyorange@gmail.com>
2021-01-01 09:02:55 +0000

    created file object - unfinished alterations

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,20 +4,46 @@ $(document).ready(function()
     var file_changed = false;
 
     // check for file in localstorage
-    check_local_storage();
+    file_id = localStorage.getItem('current_file_id');
+    if (file_id) {
+        file = new File();
+        file.loadFromServer(id);
+    }
+    else {
+        console.log("No file stored locally.")
+        file = new File($('ul#userdocument'),$('span#filename'));
+    }
 
     // new file button
     $('a#newFileButton').click( (e) => {
         e.preventDefault();
         console.log("New file button clicked")
         if (file_changed) {
-            saveChanges();
+            // show save changes modal dialog if true
+            $('#savecurrentfiledialog').modal('show');
         } else {
             localStorage.removeItem('current_file_id');
             $('span#filename').text('untitled');
             clear_question_list();
         }
     })
+
+    // Setup buttons on save changes dialog
+    $('#savefile').click( (e) => {
+        var ids = getQuestionIDs();
+        // retrieve current file id from browser
+        var file_id = localStorage.getItem('current_file_id')
+        save_file_to_server(ids, file_id);
+    });
+    $('#discardchanges').click( (event) => {
+        // Discard changes: hide this dialog
+        $('#savecurrentfiledialog').modal('hide');
+    });
+    $('#cancelload').click( (event) => {
+        $('#savecurrentfiledialog').modal('hide');
+        $('#loadwindow').modal('hide');
+    });
+
     // Display image when li is clicked on
     $(".question").click(function()
     {
@@ -71,176 +97,170 @@ $(document).ready(function()
         }
     });
 
+    //Save file form submitted
     $('#saveForm').on('submit', function(e) {
+
         // prevent form from sending by default method
         e.preventDefault();
-        save_file_to_server();
+        var ids = getQuestionIDs();
+        // retrieve current file id from browser
+        var file_id = localStorage.getItem('current_file_id')
+        save_file_to_server(ids, file_id);
     })
 
     // ****************LOAD BUTTON CLICKED**************************************
-    $('#loadButton').click( chooseFileDialog(event) );
+    $('#loadButton').click( function(e) {
+        e.preventDefault();
+        // load button clicked: get list of files available from server
+        $.ajax({
+            url: `${$SCRIPT_ROOT}get_file_list`,
+            type: 'GET',
+            success: (data) => {
+                // fill in table with html for table body containing files list from database
+                $('#load_table_body').html(data);
+                // make file icons clickable and load file if clicked
+                $("[id^='file'").click(function (event) {
+                    var file_id = event.target.id.split('_')[1];
+                    console.log("Loading file " + file_id);
+                    load_file_from_server(file_id);
+                    // Check if current file has been changed
+                    if (file_changed) {
+                        //saveChanges();
+                    };
+                });
+                // show modal load window
+                $('#loadWindow').modal('show');
+            },
+            error: function(data) {
+                console.log("Server error.")
+                alert("Server error!");
+            }
+        })
+    });
+
+    function clear_question_list() {
+        // remove all question elements from list
+        getQuestionIDs().forEach( (item) => removeQuestion(item) )
+    }
+    
+    function getQuestionIDs() {
+        // retrieves list of integers representing questions in document
+        return $('ul#userdocument').sortable("toArray").map(parseId);
+    }
+    
+    function parseId( id ) {
+        // takes full string id and returns parsed integer id after '_'
+        return parseInt(id.split('_')[1])
+    }
+    
+    function removeQuestion(qid) {
+        // Remove question from list
+        console.log(`question ${qid} removed`)
+        // destroy cloned question element
+        $(`#document_${qid}`).remove();
+        // remove disabled class from original question element
+        $(`#question_${qid}`).removeClass('disabled');
+        // restore draggable property of original question
+        $(`#question_${qid}`).draggable('enable');
+        // File has been changed
+        file_changed = true;
+    }
+    
+    function addQuestionToList($question, $document) {
+        // adds question list item object to document list
+        // create copy of question so that original stays in list
+        // make sure copy is still clickable by setting 'withDataAndEvents' to true
+        var id = $question.attr('id');
+        console.log(`Adding question ${id} to list.`);
+        // get integer id of dropped draggable question
+        var qid = parseId( id );
+        // clone original question
+        var $question_copy = $question.clone(withDataAndEvents=true);
+        $question_copy.addClass('copy');
+        $question_copy.removeClass('active');
+        if ($question.draggable() ) {
+            // destroy draggable behaviour of original
+            $question.draggable('disable');
+        }
+        // 'gray out' original question
+        $question.addClass('disabled');
+        // set id for cloned element as document_#
+        var clone_id = ['document',qid].join('_')
+        $question_copy.attr('id',clone_id);
+        // add clone to document list
+        $document.append( $question_copy );
+        // add trash glyphicon to clone
+        const trashHTML = '<a href="#" class="trash"><span class="glyphicon glyphicon-trash"></span></a>';
+        $question_copy.append(trashHTML)
+        // set trash icon to remove item from list
+        $('.trash').click( (event) => {
+            var qid=parseId(event.currentTarget.parentElement.id);
+            removeQuestion(qid);
+        })
+    }
+
+    // ************** FILE CLASS ********************************************
+    class File {
+        constructor($list,$filename) {
+            this.name="untitled";
+            this.question_list=[];
+            this.changes_saved=false;
+            this.id = null;
+            this.$list = $list;
+            this.$filename = $filename;
+            $filename.text = this.name;
+        }
+
+        update() {
+            this.question_list = this.$list.sortable("toArray").map(parseId);
+        }
+
+        saveLocally() {
+            this.update()
+            localStorage.setItem('current_file_id',this.id);
+        }
+
+        saveToServer() {
+            // update question_list to reflect sortable list
+            this.update();
+            // serialise question list
+            var id_string = JSON.stringify(this.question_list);
+            // append question ids to form data for submission
+            data = $('#saveForm').serialize() + '&ids=' + id_string + '&file_id=' + this.id;
+            console.log(data);
+            $.post('/index', data, function(data) {
+                console.log(data);
+                $('span#filename').html(data['filename']);
+                this.id = data['file_id'];
+                // File has been saved
+                this.file_changed = false;
+            });
+        };
+
+        loadFromServer() {
+            // get filename and question_list from server with AJAX
+            $.getJSON(`${$SCRIPT_ROOT}load_file`, {
+                id: this.id
+            }, function(file) {
+                // hide modal load window
+                $('#loadWindow').modal('hide');
+                console.log("File received "+file.filename)
+                // update visible filename in document window
+                $('span#filename').text(file.filename)
+                // clear list of questions
+                clear_question_list();
+                // update question list
+                var questions = file.question_list;
+                console.log(`Adding questions to document list: ${questions}`);
+                questions.forEach( (id) => {
+                    id = "#question_" + id;
+                    $element = $(id);
+                    addQuestionToList($element,$('ul#userdocument'))
+                })
+                // store file id locally
+                localStorage.setItem('current_file_id',id);
+            });
+        }
+    }
 });
 
-function check_local_storage() {
-    file_id = localStorage.getItem('current_file_id');
-    if (file_id) {
-        load_file_from_server();
-    }
-    else {
-        console.log("No file stored locally.")
-    }
-}
-
-function clear_question_list() {
-    // remove all question elements from list
-    getQuestionIDs().forEach( (item) => removeQuestion(item) )
-}
-
-function getQuestionIDs() {
-    // retrieves list of integers representing questions in document
-    return $('ul#userdocument').sortable("toArray").map(parseId);
-}
-
-function parseId( id ) {
-    // takes full string id and returns parsed integer id after '_'
-    return parseInt(id.split('_')[1])
-}
-
-function removeQuestion(qid) {
-    // Remove question from list
-    console.log(`question ${qid} removed`)
-    // destroy cloned question element
-    $(`#document_${qid}`).remove();
-    // remove disabled class from original question element
-    $(`#question_${qid}`).removeClass('disabled');
-    // restore draggable property of original question
-    $(`#question_${qid}`).draggable('enable');
-    // File has been changed
-    file_changed = true;
-}
-
-// **********************SAVE CHANGES DIALOG****************************
-function saveChanges() {
-    // show 'save changes?' dialog
-    $('#savecurrentfiledialog').modal('show');
-    $('#savefile').click( save_file_to_server() );
-    $('#discardchanges').click( (event) => {
-        // Discard changes: hide this dialog
-        $('#savecurrentfiledialog').modal('hide');
-    });
-    $('#cancelload').click( (event) => {
-        $('#savecurrentfiledialog').modal('hide');
-        $('#loadwindow').modal('hide');
-    });
-}
-
-function chooseFileDialog(event) {
-    event.preventDefault();
-    // load button clicked: get list of files available from server
-    $.ajax({
-        url: `${$SCRIPT_ROOT}get_file_list`,
-        type: 'GET',
-        success: function(data) {
-            // fill in table with html for table body containing files list from database
-            $('#load_table_body').html(data);
-            // make file icons clickable and load file if clicked
-            $("[id^='file'").click(function(event) {
-                var file_id = event.target.id.split('_')[1]
-                console.log("Loading file " + file_id);
-                load_file_from_server(file_id);
-                // Check if current file has been changed
-                if (file_changed) {
-                    saveChanges();
-                };
-            });
-            // show modal load window
-            $('#loadWindow').modal('show');
-        },
-        error: function(data) {
-            console.log("Server error.")
-            alert("Server error!");
-        }
-    })
-}
-
-// ***************************LOAD FILE FROM SERVER**************************
-function load_file_from_server(id) {
-    // get filename and question_list from server with AJAX
-    $.getJSON(`${$SCRIPT_ROOT}load_file`, {
-        id: id
-    }, function(file) {
-        // hide modal load window
-        $('#loadWindow').modal('hide');
-        console.log("File received "+file.filename)
-        // update visible filename in document window
-        $('span#filename').text(file.filename)
-        // clear list of questions
-        clear_question_list();
-        // update question list
-        var questions = file.question_list;
-        console.log(`Adding questions to document list: ${questions}`);
-        questions.forEach( (id) => {
-            id = "#question_" + id;
-            $element = $(id);
-            addQuestionToList($element,$('ul#userdocument'))
-        })
-        // store file id locally
-        localStorage.setItem('current_file_id',id);
-    });
-}
-
-function save_file_to_server() {
-    // get question ids
-    ids = getQuestionIDs();
-    var id_string = JSON.stringify(ids);
-    console.log(id_string);
-    // retrieve current file id from browser
-    var file_id = localStorage.getItem('current_file_id')
-    console.log('Retrieved file id: '+file_id);
-    // append question ids to form data for submission
-    // data = $('#saveForm').serialize() + '&ids=[' + ids + ']';
-    data = $('#saveForm').serialize() + '&ids=' + id_string + '&file_id=' + file_id; 
-    console.log(data);
-    $.post('/index', data, function(data) {
-        console.log(data);
-        $('span#filename').html(data['filename']);
-        // store file ID in local storage so that further saves write to the same 
-        // database entry
-        localStorage.setItem('current_file_id',data['file_id']);
-        // File has been saved
-        file_changed = false;
-    });
-}
-
-function addQuestionToList($question, $document) {
-    // adds question list item object to document list
-    // create copy of question so that original stays in list
-    // make sure copy is still clickable by setting 'withDataAndEvents' to true
-    var id = $question.attr('id');
-    console.log('Dropped: ',id);
-    // get integer id of dropped draggable question
-    var qid = parseId( id );
-    // clone original question
-    var $question_copy = $question.clone(withDataAndEvents=true);
-    $question_copy.addClass('copy');
-    $question_copy.removeClass('active');
-    if ($question.draggable() ) {
-        // destroy draggable behaviour of original
-        $question.draggable('disable');
-    }
-    // 'gray out' original question
-    $question.addClass('disabled');
-    // set id for cloned element as document_#
-    var clone_id = ['document',qid].join('_')
-    $question_copy.attr('id',clone_id);
-    // add clone to document list
-    $document.append( $question_copy );
-    // add trash glyphicon to clone
-    const trashHTML = '<a href="#" class="trash"><span class="glyphicon glyphicon-trash"></span></a>';
-    $question_copy.append(trashHTML)
-    // set trash icon to remove item from list
-    $('.trash').click( (event) => {
-        var qid=parseId(event.currentTarget.parentElement.id);
-        removeQuestion(qid);
-    })
-}
